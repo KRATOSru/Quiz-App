717c6851e18b19aa1d4a8a46b8c6d891c75d84c3
vladimirKartyshov <vovan198080@inbox.ru>
2020-12-01 15:59:10 +0300

    Создание главной страницы

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
+import React, {Component} from 'react'
 import Layout from "./hoc/Layout/Layout";
+import Quiz from './containers/Quiz/Quiz'
 
-function App() {
-  return (
-    <div className="App">
-     <Layout>
-
-         <div style={{width: 400, border: '1px solid black' }}>
-             <h1>Layout works</h1>
-         </div>
-
-     </Layout>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Layout>
+                <Quiz/>
+            </Layout>
+        )
+    }
 }
 
 export default App;
