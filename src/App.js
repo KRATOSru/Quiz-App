d3f1bdeb65eee9bd6a0969fc758f2a9b5c2b7824
vladimirKartyshov <vovan198080@inbox.ru>
2020-12-01 05:51:15 +0300

    Initional project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Layout from "./hoc/Layout/Layout";
+
+function App() {
+  return (
+    <div className="App">
+     <Layout>
+
+         <div style={{width: 400, border: '1px solid black' }}>
+             <h1>Layout works</h1>
+         </div>
+
+     </Layout>
+    </div>
+  );
+}
+
+export default App;
