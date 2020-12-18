e8549562034499898e23d36cd711d139589aaf6f
vladimirKartyshov <vovan198080@inbox.ru>
2020-12-18 23:33:42 +0300

    Изменение вопроса

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,21 +4,38 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
     state = {
+        activeQuestion: 0,
         quiz: [
             {
                 question: 'What color the sky?',
-                rightAnswerId: 2,
+                rightAnswerId: 3,
+                id: 1,
                 answers: [
                     {text: 'Black', id: 1},
                     {text: 'Red', id: 2},
                     {text: 'Blue', id:3},
                     {text: 'Green', id: 4}
                 ]
+            },
+            {
+                question: 'When you was born?',
+                rightAnswerId: 1,
+                id: 2,
+                answers: [
+                    {text: '1980', id: 1},
+                    {text: '1997', id: 2},
+                    {text: '2000', id:3},
+                    {text: '2004', id: 4}
+                ]
             }
         ]
     }
     onAnswerClickHandler = (answerId) => {
         console.log('Answer Id: ', answerId)
+
+        this.setState({
+            activeQuestion: this.state.activeQuestion + 1
+        })
     }
 
     render() {
@@ -28,9 +45,11 @@ class Quiz extends Component {
                     <h1>Ответьте на все вопросы</h1>
 
                     <ActiveQuiz
-                        answers={this.state.quiz[0].answers}
-                        question={this.state.quiz[0].question}
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        question={this.state.quiz[this.state.activeQuestion].question}
                         onAnswerClick={this.onAnswerClickHandler}
+                        quizLength={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
                     />
                 </div>
             </div>
